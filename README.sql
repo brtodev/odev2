c33f93f626863d78e7ff4924e98611f18ad84b1d
brtodev <103844644+brtodev@users.noreply.github.com>
2022-06-09 20:36:26 +0300

    Update and rename README.md to README.sql

---
+++ README.sql
@@ -0,0 +1,17 @@
+# ODEV 2
+
+## 1 - film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız
+
+Select * from film
+Where  replacement_cost Between 12.99 AND 16.99
+
+## 2 actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. 
+
+
+select first_name, last_name from actor
+where first_name in ('Penelope','Nick','Ed');
+
+## 3 film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız.
+
+select * from film
+where rental_rate in ('0.99','2.99','4.99') and Where replacement_cost('12.99','15.99','28.99');
